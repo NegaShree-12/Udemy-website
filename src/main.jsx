ad5ac192637b838aedf202c9e4f0a059a5f13a09
negashree <negashree2203@gmail.com>
2025-01-16 21:36:59 +0530

    changes made in footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./pages/Navbar.jsx";
+import Footer from "./Footer.jsx";
+import TopNavbar from "./components/Topnavbar/Topnavbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <Navbar />
+    <Footer />
+    {/* <TopNavbar /> */}
   </StrictMode>
 );
