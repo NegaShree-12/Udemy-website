8684bad2af6dc27f4478290fa0747b498ace11a5
negashree <negashree2203@gmail.com>
2025-01-20 09:23:34 +0530

    Created coourselist component

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Footer from "./Footer.jsx";
 import TopNavbar from "./components/Topnavbar/Topnavbar.jsx";
 import Swiperslide from "./components/Swiper/Swiper.jsx";
 import CourseTabs from "./components/Courses/course.jsx";
+import CourseList from "./components/Courselist/Courselist.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -18,5 +19,6 @@ createRoot(document.getElementById("root")).render(
       <Swiperslide />
       <CourseTabs />
     </div>
+    <CourseList />
   </StrictMode>
 );
