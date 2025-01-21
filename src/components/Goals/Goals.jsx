e1bece33ac2ee39248dc3e63fd753be418f085f3
varsa-varniga <varsavarniga05@gmail.com>
2025-01-21 22:24:52 +0530

    added price and plan section

---
+++ src/components/Goals/Goals.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Goals.css";
+
+function Goals() {
+  return (
+    <>
+    <section className="goal-container">
+    <div className="goals-section">
+      <div className="Goals-text">
+      <p className="Goals-title">Learning focused on your goals</p>
+      <div className="goal active">
+        <h2>Hands-on training</h2>
+        <p>Upskill effectively with AI-powered coding exercises, practice tests, and quizzes.</p>
+      </div>
+      <div className="goal">
+        <h2>Certification prep</h2>
+        <p>Prep for industry-recognized certifications by solving real-world challenges and earning badges along the way.</p>
+        <a href="#">Explore courses &rarr;</a>
+      </div>
+      <div className="goal">
+        <h2>
+          Insights and analytics <span className="badge">Enterprise Plan</span>
+        </h2>
+        <p>
+          Fast-track goals with advanced insights plus a dedicated customer success team to help drive effective learning.
+        </p>
+        <a href="#">Find out more &rarr;</a>
+      </div>
+      <div className="goal">
+        <h2>
+          Customizable content <span className="badge">Enterprise Plan</span>
+        </h2>
+        <p>
+          Create tailored learning paths for team and organization goals and even host your own content and resources.
+        </p>
+        <a href="#">Find out more &rarr;</a>
+      </div>
+      </div>
+
+      <div className="Goals-image">
+        <img src="udemy-containerization.png" alt="score board" />
+      </div>
+      </div>
+    </section>
+    </>
+  );
+}
+
+export default Goals;
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./pages/Navbar.jsx";
 import Footer from "./Footer.jsx";
+import Goals from "./components/Goals/Goals.jsx";
 import TopNavbar from "./components/Topnavbar/Topnavbar.jsx";
 import Swiperslide from "./components/Swiper/Swiper.jsx";
+import Subscription from "./components/Subscription/Subscription.jsx";
+import Review from "./components/Review/Review.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,5 +19,10 @@ createRoot(document.getElementById("root")).render(
     <div className="center-container">
       <Swiperslide />
     </div>
+    <Goals/>
+    <Subscription/>
+    <Review />
+    
+
   </StrictMode>
 );
