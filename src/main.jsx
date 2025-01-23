9052b3622d084295304c5e86b84dc5d27fbc9973
negashree <negashree2203@gmail.com>
2025-01-23 19:12:32 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import Navbar from "./pages/Navbar.jsx";
-import Footer from "./Footer.jsx";
 import TopNavbar from "./components/Topnavbar/Topnavbar.jsx";
 import Swiperslide from "./components/Swiper/Swiper.jsx";
 import CourseTabs from "./components/Courses/course.jsx";
@@ -11,9 +8,6 @@ import CourseList from "./components/Courselist/Courselist.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <Navbar />
-    <Footer /> */}
     <TopNavbar />
     <div className="center-container">
       <Swiperslide />
