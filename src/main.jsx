abe6be02c9d39c35215039618701e26010588305
negashree <negashree2203@gmail.com>
2025-01-26 22:49:49 +0530

    updated navbar and swiper

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,32 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Goals from "./components/Goals/Goals.jsx";
 import TopNavbar from "./components/Topnavbar/Topnavbar.jsx";
-import Swiperslide from "./components/Swiper/Swiper.jsx";
 import Trends from "./components/Trends/Trends.jsx";
 import Subscription from "./components/Subscription/Subscription.jsx";
 import Review from "./components/Review/Review.jsx";
 import CourseTabs from "./components/Courses/course.jsx";
 import CourseList from "./components/Courselist/Courselist.jsx";
+import CourseSlider from "./components/Swiper/Swiper.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <TopNavbar />
-    <div className="center-container">
-      <Swiperslide />
-      <CourseTabs />
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100px", // Full viewport height for proper centering
+        background: "linear-gradient(to bottom, #3b6035 50%, #FFFFFF 50%)", // Half green, half white
+      }}
+    >
+      <TopNavbar />
     </div>
-    <CourseList />
+    <CourseSlider />
+    <div className="center-container">
+      <CourseTabs />
+      <CourseList />
+    </div>
+    <div className="center-container"></div>
     <Goals />
     <Subscription />
     <Review />
