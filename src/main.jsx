d2e990912f7008ed562585ecd0eeb722ed82413e
Naveen-kumar2410 <naveenkumar728224@gmail.com>
2025-01-27 19:15:35 +0530

    footer created

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./pages/Navbar.jsx";
 
+
 import Goals from "./components/Goals/Goals.jsx";
 import TopNavbar from "./components/Topnavbar/Topnavbar.jsx";
 import Trends from "./components/Trends/Trends.jsx";
@@ -13,62 +14,42 @@ import TrendingNow from "./components/Trendsnow/TrendingNow.jsx";
 import Story from "./components/Story/Story.jsx";
 import AppFooter from "./components/AppFooter/AppFooter.jsx";
 
-
 import CourseTabs from "./components/Courses/course.jsx";
 import CourseList from "./components/Courselist/Courselist.jsx";
 import CourseSlider from "./components/Swiper/Swiper.jsx";
 import CommunityStats from "./components/Communitystatus/communitystatus.jsx";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <TopNavbar />
     <div className="center-container">
-      <Swiperslide />
-    </div>
-    
-    <Goals/>
-    <Subscription/>
-    <Review />
-    <Trends/>
-    <TrendingNow/>
-    <Story/>
-    <AppFooter/>
-    
-    
-      
       <CourseTabs />
       <CourseList />
-      <Goals/>
-    <Subscription/>
-    <Review />
-    <Trends/>
     </div>
-   
-  
 
-    
+    <Goals />
+    <Subscription />
+    <Review />
+    <Trends />
+    <TrendingNow />
+    <Story />
+    <AppFooter />
+
     <div
       style={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        height: "100px", // Full viewport height for proper centering
-        background: "linear-gradient(to bottom, #3b6035 50%, #FFFFFF 50%)", // Half green, half white
+        height: "100px",
+        background: "linear-gradient(to bottom, #3b6035 50%, #FFFFFF 50%)",
       }}
     >
       <TopNavbar />
     </div>
+
     <CourseSlider />
     <CommunityStats />
-    <div className="center-container">
-      <CourseTabs />
-      <CourseList />
-    </div>
-    <div className="center-container"></div>
-    <Goals />
-    <Subscription />
-    <Review />
-    <Trends />
   </StrictMode>
 );
+
+
